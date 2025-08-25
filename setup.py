8eac88740ecc779fa861b69aaab7753013c0f1bb
Jimmy0117 <jimmy930117h@gmail.com>
2025-08-25 23:22:13 +0800

    modified:   setup.py to fix the file download links.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+import os
 import requests
 
 def download_from_dropbox(shared_url, dest_path):
+    
     # 轉換成直接下載連結
     download_url = shared_url.replace('www.dropbox.com', 'dl.dropboxusercontent.com')
     download_url = download_url.replace('?dl=0', '').replace('?dl=1', '')
@@ -18,29 +20,30 @@ def __main__():
         'https://www.dropbox.com/scl/fi/r416xd5bv3dar8nmgj7m4/ffprobe.exe?rlkey=vbcxg80zatmi0jxgwjitwjll4&st=sqf3zonh&dl=0',
         {'Models': [
             'https://www.dropbox.com/scl/fi/jx1c2y11r2amh76wv0684/function_pred_SVM.py?rlkey=f2nw9qac3q0mtqce07kpwh98q&st=vt3jz20y&dl=0',
-			'https://www.dropbox.com/scl/fi/1use7ny76k8ymily981mb/best_svm_model.pkl?rlkey=hy9sh8vr9y0ab7gvr07xb6rg8&st=rdxjsy1s&dl=0',
+            'https://www.dropbox.com/scl/fi/1use7ny76k8ymily981mb/best_svm_model.pkl?rlkey=hy9sh8vr9y0ab7gvr07xb6rg8&st=rdxjsy1s&dl=0',
             'https://www.dropbox.com/scl/fi/orrcyt3go73b9itaq7grl/function_pred.py?rlkey=08crbeft8frr577aefjk6o9xm&st=q3xhrwlg&dl=0',
             'https://www.dropbox.com/scl/fi/p5berqdxlz0jnj623nnak/smote_cnn_model_0.86.h5?rlkey=8osxu647es9ekp6sfa791frs2&st=qk3qr45z&dl=0',
             'https://www.dropbox.com/scl/fi/9rjmsq7m26yxw9bdsal8s/function_pred.py?rlkey=o8ye41ptwqjprszztv2cnut1y&st=gp5a9kiv&dl=0',
             'https://www.dropbox.com/scl/fi/mmzecfr378l0czwfkpkty/smote_non_cnn_model_0.84.h5?rlkey=6bhoqqdgpj532h5g8ifd1uv6z&st=srt3mpiu&dl=0',
         ]}
-        # 這裡可以放多個 Dropbox 連結
+# 這裡可以放多個 Dropbox 連結
     ]
     for url in url_list:
         if isinstance(url, str):
             print(f"Downloading from {url}...")
-            dest_path = 'd:/MVPA2.0_interface/' + url.split('/')[-1].split('?')[0]
+            dest_path = url.split('/')[-1].split('?')[0]
             download_from_dropbox(url, dest_path)
             print(f"Saved to {dest_path}")
         elif isinstance(url, dict):
             for key in url:
+                os.makedirs(key, exist_ok=True)
                 i = 1
                 for link in url[key]:
                     print(f"Downloading from {link}...")
-                    dest_path = 'd:/MVPA2.0_interface/' + f'{key}/{i}_' + link.split('/')[-1].split('?')[0]
+                    dest_path = f'./{key}/{i}_' + link.split('/')[-1].split('?')[0]
                     download_from_dropbox(link, dest_path)
                     print(f"Saved to {dest_path}")
-                    i+=1
+                    i += 1
 
 if __name__ == "__main__":
     __main__()
